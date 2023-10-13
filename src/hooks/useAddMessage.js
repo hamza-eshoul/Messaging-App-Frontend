e9173a7f17fead0299958c813643a3421b522b22
Hamza Eshoul <hamza.eshoul.pro@gmail.com>
2023-10-13 14:44:04 +0100

    Updated the fetch URLs from  development URLs to production URLs

--- src/hooks/useAddMessage.js
+++ src/hooks/useAddMessage.js
@@ -7,7 +7,7 @@ export const useAddMessage = () => {
 
   const addMessage = async (messageInfo) => {
     const response = await fetch(
-      "http://localhost:4000/conversation/add_message",
+      "https://odin-messaging-app-api.onrender.com/conversation/add_message",
       {
         method: "POST",
         headers: {
@@ -17,7 +17,7 @@ export const useAddMessage = () => {
         body: JSON.stringify({
           ...messageInfo,
         }),
-      }
+      },
     );
 
     const json = await response.json();
